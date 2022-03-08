16f73fe753a43aa275de166db6cd68b8bfac3b62
ManhAn9077 <94302143+ManhAn9077@users.noreply.github.com>
2022-03-08 10:35:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị Phương';
+const giftUrl = 'https://www.tiktok.com/@tonghopcliphott/video/7070974053938548014?is_copy_url=1&is_from_webapp=v1&q=8%2F3&t=1646704928405';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = '8-3 ý nghĩa';
+const contentCard = 'Nhân dịp 8-3 này em xin gửi lời chúc đến chị và mẹ chị có 1 ngày thật ý nghĩa, thật vui vẻ. Chúc chị đạt được mọi điều mình đặt ra trong năm nay. 8-3 vui vẻ ạ ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'chị phương.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
